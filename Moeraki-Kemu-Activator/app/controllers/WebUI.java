a14a1efca222a34dd2e05e1efd4212101330de54
Etienne Gramlich <etgramlich@gmx.de>
2016-10-26 15:25:01 +0200

    StartGame isnt hanging anymore

--- Moeraki-Kemu-Activator/app/controllers/WebUI.java
+++ Moeraki-Kemu-Activator/app/controllers/WebUI.java
@@ -19,7 +19,7 @@ import views.html.*;
 @Singleton
 public class WebUI extends Controller {
 	
-	IController controller = null;
+	private IController controller = null;
 	
 	public String newGame(final String player1Name, final String player2Name) {
 		// Create new game
@@ -41,29 +41,11 @@ public class WebUI extends Controller {
 		IControllerPlayer playerController = new ControllerPlayer();
 		controller = new de.htwg.se.moerakikemu.controller.controllerimpl.Controller(8, playerController);
 	
-		UserInterface[] interfaces = new UserInterface[2];
-		interfaces[0] = injector.getInstance(TextUI.class);
-		//interfaces[1] = injector.getInstance(GUI.class);
+		UserInterface tui = injector.getInstance(TextUI.class);
 
-		for (int i = 0; i < 1/*interfaces.length*/; i++) {
-			((IObserverSubject) controller).attatch((ObserverObserver) interfaces[i]);
-			interfaces[i].drawCurrentState();
-		}
+		((IObserverSubject) controller).attatch((ObserverObserver) tui);
+		tui.drawCurrentState();
 
-		// Used to query Player names
-		//((ObserverObserver) interfaces[1]).update();
-		
-		boolean finished = false;
-		while (!finished) {
-			finished = controller.testIfEnd();
-			interfaces[0].update();
-			//interfaces[1].update();
-		}
-		
-		for (UserInterface ui : interfaces) {
-			ui.quit();
-		}
-		
 		return ok("Game created");
 	}
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+Moeraki Kemu Web Frontend
+---------
+
+This is a web frontend for my implementation of the board game Moeraki Kemu that you can find [here](https://github.com/etgramli/Moeraki_Kemu).
+The linked project has to be exported as executable jar (i.e. by eclipse) to be used in this project.
+
+The web ui is achieved with the Play framework.
