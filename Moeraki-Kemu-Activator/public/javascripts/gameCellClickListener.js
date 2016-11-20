39b0f60dcd485699ab0dee39e1b7bcb4e4f85219
Etienne Gramlich <etgramlich@gmx.de>
2016-11-20 09:54:51 +0100

    Refactoring (HTML without javascript)

--- Moeraki-Kemu-Activator/public/javascripts/gameCellClickListener.js
+++ Moeraki-Kemu-Activator/public/javascripts/gameCellClickListener.js
@@ -1,8 +1,32 @@
-var list = document.getElementsByTagName("th");
-function changeinnerhtml() {
-    var classname = this.className;
-    this.innerHTML = "onclick'd";
-}
-for (i = 0, len = list.length; i < len; i++){
-    list[i].onclick = changeinnerhtml;
-}
+$(document).ready(function() {
+            $('.gameCell').click(function(evt) {
+                $.ajax({
+                    type : 'POST',
+                    url : "/occ",
+                    contentType: "text/json",
+                    data : JSON.stringify($(this).attr("id")),
+                    success : function(data) {
+                        var json = JSON.parse(data)
+                        var lines = json.lines;
+                        for (var i = 0; i < lines.length; i++) {
+                            var line = lines[i];
+                            var cells = line.cells
+                            for (var j = 0; j < cells.length; j++) {
+                                var cell = cells[j];
+                                if (cell == "StartDot") {
+                                    $('#' + i + '-' + j).css('background-color', '#000000');
+                                } else if (cell == "Spieler 1") {
+                                    $('#' + i + '-' + j).css('background-color', '#00FF11');
+                                } else if (cell == "Spieler 2") {
+                                    $('#' + i + '-' + j).css('background-color', '#FF3300');
+                                }
+                            }
+                        }
+                    },
+                    error : function(data) {
+                        $('#test').html("Failure" + data);
+                    }
+                });
+                return false;
+            });
+        });
