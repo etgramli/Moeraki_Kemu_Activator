849e995b66092c3fffb5837212990f7284a39033
etgramli <etgramlich@gmx.de>
2016-11-23 14:58:23 +0100

    Own LegacyWebSocket class

---
+++ Moeraki-Kemu-Activator/app/controllers/LegacyWebSocketController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import play.mvc.LegacyWebSocket;
+
+public class LegacyWebSocketController {
+	private StringWebSocket ws;
+	
+	public LegacyWebSocketController() {
+		ws = new StringWebSocket();
+	}
+}
---
+++ Moeraki-Kemu-Activator/app/controllers/StringWebSocket.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import play.mvc.LegacyWebSocket;
+import play.mvc.WebSocket.In;
+import play.mvc.WebSocket.Out;
+
+public class StringWebSocket extends LegacyWebSocket<String> {
+
+	@Override
+	public void onReady(In<String> arg0, Out<String> arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
